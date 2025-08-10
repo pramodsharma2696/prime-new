15e817496e4f1276983caef8f9bc77912c5f7e2f
pramod sharma <pramodsharma2696@gmail.com>
2025-08-10 22:39:48 +0530

    commit it

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -38,19 +38,7 @@
       gtag("config", "G-04K4CDYX9T");
     </script>
     <!--Start of Tawk.to Script-->
-    <script type="text/javascript">
-      var Tawk_API = Tawk_API || {},
-        Tawk_LoadStart = new Date();
-      (function () {
-        var s1 = document.createElement("script"),
-          s0 = document.getElementsByTagName("script")[0];
-        s1.async = true;
-        s1.src = "https://embed.tawk.to/6579c32c07843602b801ae1f/1hhhq4pgq";
-        s1.charset = "UTF-8";
-        s1.setAttribute("crossorigin", "*");
-        s0.parentNode.insertBefore(s1, s0);
-      })();
-    </script>
+   
     <!--End of Tawk.to Script-->
   </head>
   <body>
